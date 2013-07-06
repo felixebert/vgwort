3c6ce43dce2ad5a5be33ceb625219ab3b3cdc4ca
Felix Ebert <felix.ebert@if-core.de>
2013-07-06 23:07:44 +0200

    message request

--- src/main/java/de/schreibfabrik/vgwort/PixelRequest.java
+++ src/main/java/de/schreibfabrik/vgwort/PixelRequest.java
@@ -13,6 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
+import de.schreibfabrik.vgwort.soap.message.Authors;
+import de.schreibfabrik.vgwort.soap.message.Involved;
+import de.schreibfabrik.vgwort.soap.message.Message;
+import de.schreibfabrik.vgwort.soap.message.MessageService;
+import de.schreibfabrik.vgwort.soap.message.MessageText;
+import de.schreibfabrik.vgwort.soap.message.NewMessageFault_Exception;
+import de.schreibfabrik.vgwort.soap.message.NewMessageRequest;
+import de.schreibfabrik.vgwort.soap.message.NewMessageResponse;
+import de.schreibfabrik.vgwort.soap.message.Parties;
+import de.schreibfabrik.vgwort.soap.message.Text;
 import de.schreibfabrik.vgwort.soap.pixel.OrderPixelFault_Exception;
 import de.schreibfabrik.vgwort.soap.pixel.OrderPixelRequest;
 import de.schreibfabrik.vgwort.soap.pixel.OrderPixelResponse;
@@ -60,7 +70,8 @@ public class PixelRequest
 			System.out.println(pixelResponse.getPixels().getPixel());
 			for (Pixel_Type pixel : pixelResponse.getPixels().getPixel())
 			{
-				System.out.println(pixel.getPublicIdentificationId());
+				System.out.println("public: " + pixel.getPublicIdentificationId());
+				System.out.println("private: " + pixel.getPrivateIdentificationId());
 			}
 		}
 		catch (OrderPixelFault_Exception e)
@@ -71,12 +82,12 @@ public class PixelRequest
 
 	public void submitText()
 	{
-		PixelService pixelService = new PixelService();
-		Pixel pixelPort = pixelService.getPixelPort();
+		MessageService messageService = new MessageService();
+		Message messagePort = messageService.getMessagePort();
 
-		Map<String, Object> req_ctx = ((BindingProvider)pixelPort).getRequestContext();
+		Map<String, Object> req_ctx = ((BindingProvider)messagePort).getRequestContext();
 		req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-				"https://tom-test.vgwort.de/services/1.0/pixelService.wsdl");
+				"https://tom-test.vgwort.de/services/1.0/messageService.wsdl");
 
 		Map<String, List<String>> headers = new HashMap<String, List<String>>();
 		String base64String = Base64.encode((metisConfig.getUser() + ":" + metisConfig.getPassword()).getBytes());
@@ -85,20 +96,27 @@ public class PixelRequest
 
 		try
 		{
-			OrderPixelRequest orderPixelRequest = new OrderPixelRequest();
-			orderPixelRequest.setCount(1);
+			MessageText messageText = new MessageText();
+			messageText.setShorttext("Lösung von GTA IV");
+			messageText.setLyric(false);
+			Text text = new Text();
+			text.setPlainText(Base64.encode("test".getBytes()).getBytes());
+			messageText.setText(text);
 
-			OrderPixelResponse pixelResponse = pixelPort.orderPixel(orderPixelRequest);
+			Involved involved = new Involved();
+			Authors authors = new Authors();
+			authors.getAuthor().add(involved);
+			Parties parties = new Parties();
 
-			System.out.println(pixelResponse);
-			System.out.println(pixelResponse.getPixels());
-			System.out.println(pixelResponse.getPixels().getPixel());
-			for (Pixel_Type pixel : pixelResponse.getPixels().getPixel())
-			{
-				System.out.println(pixel.getPublicIdentificationId());
-			}
+			NewMessageRequest messageRequest = new NewMessageRequest();
+			messageRequest.setPrivateidentificationid("aa5577ed95c745cf9c4f77a5e9220505");
+			messageRequest.setMessagetext(messageText);
+
+			NewMessageResponse messageResponse = messagePort.newMessage(messageRequest);
+			System.out.println(messageResponse);
+			System.out.println(messageResponse.getStatus());
 		}
-		catch (OrderPixelFault_Exception e)
+		catch (NewMessageFault_Exception e)
 		{
 			e.printStackTrace();
 		}
