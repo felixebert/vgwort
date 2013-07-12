d0f2cc93250148d60990cddc81d7a244d1cb1efe
Felix Ebert <felix.ebert@if-core.de>
2013-07-12 16:24:27 +0200

    removed java app

--- src/main/java/de/schreibfabrik/vgwort/soap/pixel/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://vgwort.de/1.0/PixelService/xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.schreibfabrik.vgwort.soap.pixel;
