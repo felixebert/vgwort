539ffd15ab03c71efb24beaf9d30677c56577f45
Felix Ebert <felix.ebert@if-core.de>
2013-07-27 05:10:40 +0200

    removed applicationContext

--- src/test/java/de/ifcore/metis/client/IntegrationTest.java
+++
@@ -1,8 +0,0 @@
-package de.ifcore.metis.client;
-
-import org.springframework.test.context.ContextConfiguration;
-
-@ContextConfiguration(locations = { "/applicationContext.xml" })
-public abstract class IntegrationTest
-{
-}
