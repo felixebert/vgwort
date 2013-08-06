792175f5ff55b3581101a66f741c0c610c45ab80
Felix Ebert <felix.ebert@if-core.de>
2013-08-06 17:49:03 +0200

    PixelClientIntegrationTest

---
+++ src/test/java/de/ifcore/metis/client/pixel/PixelClientIntegrationTest.java
@@ -0,0 +1,27 @@
+package de.ifcore.metis.client.pixel;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.ifcore.metis.client.MetisEndpoint;
+
+public class PixelClientIntegrationTest
+{
+	private static final Logger log = LoggerFactory.getLogger(PixelClientIntegrationTest.class);
+
+	@Test
+	public void testOrder()
+	{
+		MetisEndpoint endpoint = new MetisEndpoint("tom-test.vgwort.de", "xx", "yy");
+		PixelClient client = new PixelClient(endpoint);
+
+		List<Pixel> pixels = client.order(1);
+		assertEquals(1, pixels.size());
+		log.debug(pixels.iterator().next().toString());
+	}
+}
