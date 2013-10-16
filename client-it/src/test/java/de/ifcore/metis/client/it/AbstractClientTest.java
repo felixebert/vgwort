85314520d41f97218650bbe66197b807baf338f5
Felix Ebert <felix.ebert@if-core.de>
2013-10-16 03:34:25 +0200

    it: AbstractClientTest for providing metis auth info

---
+++ client-it/src/test/java/de/ifcore/metis/client/it/AbstractClientTest.java
@@ -0,0 +1,33 @@
+package de.ifcore.metis.client.it;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.BeforeClass;
+
+public abstract class AbstractClientTest
+{
+	protected static String USER;
+	protected static String PASSWORD;
+
+	@BeforeClass
+	public static void setProperties() throws IOException
+	{
+		if (System.getProperty("metis.user") != null)
+		{
+			USER = System.getProperty("metis.user");
+			PASSWORD = System.getProperty("metis.pass");
+		}
+		else
+		{
+			Properties prop = new Properties();
+			prop.load(AbstractClientTest.class.getClassLoader().getResourceAsStream("metis.properties"));
+			USER = prop.getProperty("metis.user");
+			PASSWORD = prop.getProperty("metis.pass");
+			if ("true".equals(prop.getProperty("jaxws.dump")))
+			{
+				System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+			}
+		}
+	}
+}
--- client-it/src/test/java/de/ifcore/metis/client/it/PixelClientTest.java
+++ client-it/src/test/java/de/ifcore/metis/client/it/PixelClientTest.java
@@ -1,25 +1,32 @@
-package de.ifcore.metis.client.pixel;
+package de.ifcore.metis.client.it;
 
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 import de.ifcore.metis.client.MetisEndpoint;
+import de.ifcore.metis.client.pixel.Pixel;
+import de.ifcore.metis.client.pixel.PixelClient;
 
-public class PixelClientIntegrationTest
+public class PixelClientTest extends AbstractClientTest
 {
+	private MetisEndpoint endpoint;
+
+	@Before
+	public void setup()
+	{
+		endpoint = new MetisEndpoint("tom-test.vgwort.de", USER, PASSWORD);
+	}
+
 	@Test
-	@Ignore
 	public void testOrder()
 	{
-		MetisEndpoint endpoint = new MetisEndpoint("tom-test.vgwort.de", "user", "pw");
 		PixelClient client = new PixelClient(endpoint);
 
 		List<Pixel> pixels = client.order(1);
 		assertEquals(1, pixels.size());
-		System.out.println(pixels.iterator().next().toString());
 	}
 }
