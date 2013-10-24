fdd439d59bb5238654d52a70cb184d3467b45340
Felix Ebert <felix.ebert@if-core.de>
2013-10-24 22:42:27 +0200

    test exception handling of PixelClient

--- client-it/src/test/java/de/ifcore/metis/client/it/PixelClientTest.java
+++ client-it/src/test/java/de/ifcore/metis/client/it/PixelClientTest.java
@@ -8,25 +8,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 import de.ifcore.metis.client.MetisEndpoint;
+import de.ifcore.metis.client.exception.MetisException;
 import de.ifcore.metis.client.pixel.Pixel;
 import de.ifcore.metis.client.pixel.PixelClient;
 
 public class PixelClientTest extends AbstractClientTest
 {
-	private MetisEndpoint endpoint;
+	private PixelClient client;
 
 	@Before
 	public void setup()
 	{
-		endpoint = new MetisEndpoint("tom-test.vgwort.de", USER, PASSWORD);
+		client = new PixelClient(new MetisEndpoint("tom-test.vgwort.de", USER, PASSWORD));
 	}
 
 	@Test
-	public void testOrder()
+	public void shouldOrderPixels()
 	{
-		PixelClient client = new PixelClient(endpoint);
-
 		List<Pixel> pixels = client.order(1);
 		assertEquals(1, pixels.size());
+	}
+
+	@Test(expected = MetisException.class)
+	public void shouldHandleBusinessError()
+	{
+		client.order(Integer.MAX_VALUE);
+	}
+
+	@Test(expected = MetisException.class)
+	public void shouldHandleValidationError()
+	{
+		client.order(-1);
 	}
 }
